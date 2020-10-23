b13e8231d4b6139fa8aada7ea0cf3250b80356d7
rkogeyama <rkogeyama@gmail.com>
2020-10-23 19:07:26 +0000

    syntax correction in clean uspatentcitation chunks

--- clean_uspatentcitation_chunk.py
+++ clean_uspatentcitation_chunk.py
@@ -3,18 +3,19 @@ import pandas as pd
 #import gzip
 import re
 import csv
+import dask.dataframe as dd
 
 dst='data/cleanuspatentcitation.csv.gz'
 file="data/uspatentcitation.tsv.zip"
-df=pd.read_csv(file, compression='zip', sep="\t", chunksize=10000, error_bad_lines=False, encoding="utf-8")
-cleaning_patent=lambda x:re.sub('([^a-zA-Z0-9]+)', "", x)
+df=dd.read_csv(file, compression='zip', sep="\t", blocksize=1024*1024*16, error_bad_lines=False, encoding="utf-8")
 # first_patent = datetime.date(1790, 7, 31)
 first_patent = pd.to_datetime('1790-06-30', format="%Y-%m-%d") #helps us to identify citations with problems - small change from the actual first patent's grant date because one of the citations for n1 seems to be right
-df clean(df):
+def clean(df):
+    cleaning_patent=lambda x:re.sub('([^a-zA-Z0-9]+)', "", x)
     df['patent_id']=df['patent_id'].apply(cleaning_patent)
-    df.date.replace({'-00':'-01'}, regex=True, inplace=True)
-    df['date']=pd.to_datetime(chunk['date'], format="%Y-%m-%d", errors='coerce', infer_datetime_format=True)
-    df.dropna(subset=['date'], inplace=True)
+    df['date']=df['date'].mask(df['date']=='-00','-01')
+    #df['date']=pd.to_datetime(chunk['date'], format="%Y-%m-%d", errors='coerce', infer_datetime_format=True)
+    #df.dropna(subset=['date'], inplace=True)
     df.to_csv(dst, mode='a', compression='gzip')
     
 result=clean(df)
