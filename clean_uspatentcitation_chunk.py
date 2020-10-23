9742082f68dcb261e12191a03111c34ffe452065
rkogeyama <rkogeyama@gmail.com>
2020-10-23 15:43:06 -0300

    added multithread to clean_uspatentcitation_chunk

--- clean_uspatentcitation_chunk.py
+++ clean_uspatentcitation_chunk.py
@@ -10,9 +10,12 @@ df=pd.read_csv(file, compression='zip', sep="\t", chunksize=10000, error_bad_lin
 cleaning_patent=lambda x:re.sub('([^a-zA-Z0-9]+)', "", x)
 # first_patent = datetime.date(1790, 7, 31)
 first_patent = pd.to_datetime('1790-06-30', format="%Y-%m-%d") #helps us to identify citations with problems - small change from the actual first patent's grant date because one of the citations for n1 seems to be right
-for chunk in df:
-    chunk['patent_id']=chunk['patent_id'].apply(cleaning_patent)
-    chunk.date.replace({'-00':'-01'}, regex=True, inplace=True)
-    chunk['date']=pd.to_datetime(chunk['date'], format="%Y-%m-%d", errors='coerce', infer_datetime_format=True)
-    chunk.dropna(subset=['date'], inplace=True)
-    chunk.to_csv(dst, mode='a', compression='gzip')
+df clean(df):
+    df['patent_id']=df['patent_id'].apply(cleaning_patent)
+    df.date.replace({'-00':'-01'}, regex=True, inplace=True)
+    df['date']=pd.to_datetime(chunk['date'], format="%Y-%m-%d", errors='coerce', infer_datetime_format=True)
+    df.dropna(subset=['date'], inplace=True)
+    df.to_csv(dst, mode='a', compression='gzip')
+    
+result=clean(df)
+result=result.compute(num_workers=4)
