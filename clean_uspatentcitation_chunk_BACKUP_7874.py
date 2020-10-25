157dbfa101262110a554b2272e3c700a0f37b4b1
rkogeyama <rkogeyama@gmail.com>
2020-10-25 16:57:31 -0300

    clean_upatent chunk working

--- clean_uspatentcitation_chunk_BACKUP_7874.py
+++ clean_uspatentcitation_chunk_BACKUP_7874.py
@@ -3,14 +3,17 @@ import pandas as pd
 #import gzip
 import re
 import csv
-import dask.dataframe as dd
-import multiprocessing
 
-#file="data/uspatentcitation.tsv.zip"
-#dst='data/cleanuspatentcitation.csv.gz'
-file="D:\\PatentsView_2020\\uspatentcitation.tsv.zip"
-dst='D:\\PatentsView_2020\\cleanuspatentcitation.csv.gz'
+file="data/uspatentcitation.tsv.zip"
+dst='data/cleanuspatentcitation.csv.gz'
+#file="D:\\PatentsView_2020\\uspatentcitation.tsv.zip"
+#dst='D:\\PatentsView_2020\\cleanuspatentcitation.csv.gz'
+<<<<<<< HEAD:clean_uspatentcitation_chunk.py
 df=pd.read_csv(file, compression='zip', sep="\t", chunksize=10000, error_bad_lines=False, encoding="utf-8", usecols=['patent_id', 'citation_id', 'date'])
+=======
+
+df=dd.read_csv(file, compression='zip', sep="\t", error_bad_lines=False, encoding="utf-8", usecols=['patent_id', 'citation_id', 'date'])
+>>>>>>> dd does not accept chunksize, corrected:clean_uspatentcitation_dd.py
 cleaning_patent=lambda x:re.sub('([^a-zA-Z0-9]+)', "", x)
 # first_patent = datetime.date(1790, 7, 31)
 first_patent = pd.to_datetime('1790-06-30', format="%Y-%m-%d") #helps us to identify citations with problems - small change from the actual first patent's grant date because one of the citations for n1 seems to be right
@@ -19,4 +22,4 @@ for chunk in df:
     chunk.date.replace({'-00':'-01'}, regex=True, inplace=True)
     chunk['date']=pd.to_datetime(chunk['date'], format="%Y-%m-%d", errors='coerce', infer_datetime_format=True)
     chunk.dropna(subset=['date'], inplace=True)
-    chunk.to_csv(dst, mode='w', compression='gzip', encoding='utf-8')
+    chunk.to_csv(dst, mode='a', compression='gzip', encoding='utf-8')
--- clean_uspatentcitation_chunk_LOCAL_7874.py
+++ clean_uspatentcitation_chunk_LOCAL_7874.py
@@ -3,13 +3,11 @@ import pandas as pd
 #import gzip
 import re
 import csv
-import dask.dataframe as dd
-import multiprocessing
 
-#file="data/uspatentcitation.tsv.zip"
-#dst='data/cleanuspatentcitation.csv.gz'
-file="D:\\PatentsView_2020\\uspatentcitation.tsv.zip"
-dst='D:\\PatentsView_2020\\cleanuspatentcitation.csv.gz'
+file="data/uspatentcitation.tsv.zip"
+dst='data/cleanuspatentcitation.csv.gz'
+#file="D:\\PatentsView_2020\\uspatentcitation.tsv.zip"
+#dst='D:\\PatentsView_2020\\cleanuspatentcitation.csv.gz'
 df=pd.read_csv(file, compression='zip', sep="\t", chunksize=10000, error_bad_lines=False, encoding="utf-8", usecols=['patent_id', 'citation_id', 'date'])
 cleaning_patent=lambda x:re.sub('([^a-zA-Z0-9]+)', "", x)
 # first_patent = datetime.date(1790, 7, 31)
@@ -19,4 +17,4 @@ for chunk in df:
     chunk.date.replace({'-00':'-01'}, regex=True, inplace=True)
     chunk['date']=pd.to_datetime(chunk['date'], format="%Y-%m-%d", errors='coerce', infer_datetime_format=True)
     chunk.dropna(subset=['date'], inplace=True)
-    chunk.to_csv(dst, mode='w', compression='gzip', encoding='utf-8')
+    chunk.to_csv(dst, mode='a', compression='gzip', encoding='utf-8')
