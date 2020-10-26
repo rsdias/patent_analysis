61988444386a912276ba3d73fa423accacb337e3
rkogeyama <rkogeyama@gmail.com>
2020-10-26 13:58:44 -0300

    adjusted dask script clean_uspatentcitaition_dd

--- clean_uspatentcitation_dd.py
+++ clean_uspatentcitation_dd.py
@@ -15,17 +15,19 @@ first_patent = pd.to_datetime('1790-06-30', format="%Y-%m-%d") #helps us to iden
 # first_patent = datetime.date(1790, 7, 31)
 df = [delayed(pd.read_csv)(f, compression='zip', usecols=['patent_id', 'citation_id', 'date'], dtype=object, sep="\t", error_bad_lines=False, encoding="utf-8") for f in file_list]
 
-
-def cleaning(df): 
-    cleaning_patent=lambda x:re.sub('([^a-zA-Z0-9]+)', "", x)
-    df['patent_id']=df['patent_id'].apply(cleaning_patent, meta=pd.Series(dtype='object', name='patent_id'))
-    correct_date=lambda x:re.sub('-00', "-01", x)
-    df['date']=df['date'].apply(correct_date, meta=pd.Series(dtype='object', name='date'))
-    #df['date']=pd.to_datetime(df['date'], format="%Y-%m-%d", errors='coerce', infer_datetime_format=True)
-    #df.dropna(subset=['date'], inplace=True)
-    df.to_csv(dst, mode='w', compression='gzip', encoding='utf-8')
-
 myTypes={'patent_id':object, 'citation_id':object, 'date':object}
-ddf = dd.from_delayed(df, meta=myTypes)
-result=cleaning(ddf)
-result=result.compute(num_workers=12)
+df = dd.from_delayed(df, meta=myTypes)
+
+cleaning_patent=lambda x:re.sub('([^a-zA-Z0-9]+)', "", x)
+df['patent_id']=df['patent_id'].apply(cleaning_patent, meta=pd.Series(dtype='object', name='patent_id'))
+
+correct_date=lambda x:re.sub('-00', "-01", x)
+df['date']=df['date'].apply(correct_date, meta=pd.Series(dtype='object', name='date'))
+
+#df['date']=pd.to_datetime(df['date'], format="%Y-%m-%d", errors='coerce', infer_datetime_format=True)
+#df.dropna(subset=['date'], inplace=True)
+
+df.to_csv(dst, mode='w', compression='gzip', encoding='utf-8')
+
+result=client.persist(df)
+#result=result.compute(num_workers=12)
