196afbe92f25b6414e8394458743316c308efbe5
rkogeyama <rkogeyama@gmail.com>
2020-10-26 13:58:44 -0300

    adjusted dask script clean_uspatentcitaition_dd

--- clean_uspatentcitation_chunk.py
+++ clean_uspatentcitation_chunk.py
@@ -10,7 +10,7 @@ file="data/uspatentcitation.tsv.zip"
 dst='data/cleanuspatentcitation.csv.gz'
 #file="D:\\PatentsView_2020\\uspatentcitation.tsv.zip"
 #dst='D:\\PatentsView_2020\\cleanuspatentcitation.csv.gz'
-df=pd.read_csv(file, compression='zip', sep="\t", chunksize=10000, error_bad_lines=False, encoding="utf-8", usecols=['patent_id', 'citation_id', 'date'])
+df=pd.read_csv(file, compression='zip', sep="\t", chunksize=1024*1024, error_bad_lines=False, encoding="utf-8", usecols=['patent_id', 'citation_id', 'date'])
 cleaning_patent=lambda x:re.sub('([^a-zA-Z0-9]+)', "", x)
 # first_patent = datetime.date(1790, 7, 31)
 first_patent = pd.to_datetime('1790-06-30', format="%Y-%m-%d") #helps us to identify citations with problems - small change from the actual first patent's grant date because one of the citations for n1 seems to be right
