f5a2080e8cec4aa56f56842b3262f26e88280e06
rkogeyama <rkogeyama@gmail.com>
2020-10-27 18:55:12 -0300

    updated clean uspatentcitation dd

--- clean_uspatentcitation_dd.py
+++ clean_uspatentcitation_dd.py
@@ -6,6 +6,12 @@ import csv
 import dask.dataframe as dd
 from dask.delayed import delayed
 
+def clean_field(df, field):
+    cleaning_patent=lambda x:re.sub('([^a-zA-Z0-9]+)', "", x)
+    #df['patent_id']=df['patent_id'].apply(cleaning_patent, meta=pd.Series(dtype='object', name='patent_id'))
+    df[field]=df[field].apply(cleaning_patent)
+    return df
+    
 file_list="data/uspatentcitation.tsv.zip"
 dst='data/cleanuspatentcitation.csv.gz'
 #file="D:\\PatentsView_2020\\uspatentcitation.tsv.zip"
@@ -13,13 +19,12 @@ dst='data/cleanuspatentcitation.csv.gz'
 
 first_patent = pd.to_datetime('1790-06-30', format="%Y-%m-%d") #helps us to identify citations with problems - small change from the actual first patent's grant date because one of the citations for n1 seems to be right
 # first_patent = datetime.date(1790, 7, 31)
-df = dask.delayed(pd.read_csv (file_list, compression='zip', usecols=['patent_id', 'citation_id', 'date'], dtype=object, sep="\t", error_bad_lines=False, encoding="utf-8")) 
+df = delayed(pd.read_csv)(file_list, compression='zip', usecols=['patent_id', 'citation_id', 'date'], dtype=object, sep="\t", error_bad_lines=False, encoding="utf-8")
 
 #myTypes={'patent_id':object, 'citation_id':object, 'date':object}
 #df = dd.from_delayed(df, meta=myTypes)
 
-cleaning_patent=lambda x:re.sub('([^a-zA-Z0-9]+)', "", x)
-df['patent_id']=df['patent_id'].apply(cleaning_patent, meta=pd.Series(dtype='object', name='patent_id'))
+df=delayed(clean_field)(df, 'patent_id')
 
 #correct_date=lambda x:re.sub('-00', "-01", x)
 #df['date']=df['date'].apply(correct_date, meta=pd.Series(dtype='object', name='date'))
@@ -28,6 +33,6 @@ df['patent_id']=df['patent_id'].apply(cleaning_patent, meta=pd.Series(dtype='obj
 #df.dropna(subset=['date'], inplace=True)
 
 #result=client.persist(df)
-result=df.compute(num_workers=8)
+df=df.compute(num_workers=8)
 
-result.to_parquet(dst)
+df.to_parquet(dst)
