95f3f8eac3ee983213654e45e96a5e042288d882
rkogeyama <rkogeyama@gmail.com>
2020-10-29 23:13:51 +0000

    var_builder updated with right index in merge

--- file_to_parquet.py
+++ file_to_parquet.py
@@ -6,14 +6,14 @@ import pandas as pd
 import os
 import pyarrow
 
-#file="data/uspatentcitation.tsv.zip"
-#cols=['patent_id', 'citation_id', 'date']
-file="data/patent.tsv.zip"
-cols=['id', 'num_claims', 'date', 'type', 'kind']
+file="data/uspatentcitation.tsv.zip"
+cols=['patent_id', 'citation_id', 'date']
+#file="data/patent.tsv.zip"
+#cols=['id', 'num_claims', 'date', 'type', 'kind']
 
 df = pd.read_csv(file, compression='zip', chunksize=15*1024*1024, usecols=cols, dtype=object, sep="\t", error_bad_lines=False, encoding="utf-8")
 
 for i, chunk in enumerate(df):
-    #filename="parquet/uspatentcitation"+f"{i:03d}"+".parquet.gz"
-    filename="parquet/patent"+f"{i:03d}"+".parquet.gz"
+    filename="parquet/uspatentcitation"+f"{i:03d}"+".parquet.gz"
+    #filename="parquet/patent"+f"{i:03d}"+".parquet.gz"
     chunk.to_parquet(filename, compression='gzip')
