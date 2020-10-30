1dedc1ca2deef98ea9d6a15282772dc90b3a9c12
rkogeyama <rkogeyama@gmail.com>
2020-10-30 13:04:25 -0300

    excluded kind and type from file_to_parquet patent

--- file_to_parquet.py
+++ file_to_parquet.py
@@ -10,8 +10,9 @@ import pyarrow
 #cols=['patent_id', 'citation_id', 'date']
 file="data/patent.tsv.zip"
 cols=['id', 'num_claims', 'date']
+index='id'
 
-df = pd.read_csv(file, compression='zip', chunksize=15*1024*1024, usecols=cols, dtype=object, sep="\t", error_bad_lines=False, encoding="utf-8")
+df = pd.read_csv(file, compression='zip', chunksize=15*1024*1024, usecols=cols, dtype=object, sep="\t", error_bad_lines=False, encoding="utf-8", index_col=index)
 
 for i, chunk in enumerate(df):
     #filename="parquet/uspatentcitation_"+f"{i:03d}"+".parquet.gz"
