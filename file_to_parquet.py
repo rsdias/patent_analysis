6500c3bff202eb3f632c10f526b3e3a53a299ee5
rkogeyama <rkogeyama@gmail.com>
2020-10-30 13:11:14 -0300

    excluded kind and type from file_to_parquet patent

--- file_to_parquet.py
+++ file_to_parquet.py
@@ -15,7 +15,7 @@ file="data/patent.tsv.zip"
 cols=['id', 'num_claims', 'date']
 index='id'
 
-df = pd.read_csv(file, compression='zip', chunksize=15*1024*1024, usecols=cols, dtype=object, sep="\t", error_bad_lines=False, encoding="utf-8", index_col=index)
+df = pd.read_csv(file, compression='zip', chunksize=15*1024*1024, usecols=cols, dtype=object, sep="\t", error_bad_lines=False, encoding="utf-8")
 
 for i, chunk in enumerate(df):
     #filename="parquet/uspatentcitation_"+f"{i:03d}"+".parquet.gz"
